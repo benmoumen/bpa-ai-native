208b3c009234d925adda2be6c5528b350a0377bf
Moulay Mehdi BENMOUMEN <benmoumen@gmail.com>
2026-01-01 12:12:51 +0300

    chore: update sprint status and workflow exports
    - Mark Stories 4-1 through 4-5a as done in sprint-status.yaml
    - Add workflow component barrel exports

---
+++ apps/web/src/components/workflow/index.ts
@@ -0,0 +1,15 @@
+/**
+ * Workflow Components
+ *
+ * Components for workflow configuration and management.
+ * Story 4-2: Define Workflow Steps
+ * Story 4-3: Configure Workflow Transitions
+ * Story 4-4: Specify Step Actions
+ */
+
+export { RolesList } from './RolesList';
+export { CreateRoleDialog } from './CreateRoleDialog';
+export { TransitionsList } from './TransitionsList';
+export { CreateTransitionDialog } from './CreateTransitionDialog';
+export { StepActionsPanel } from './StepActionsPanel';
+export { EditStatusDialog } from './EditStatusDialog';
